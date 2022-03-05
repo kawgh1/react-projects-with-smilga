14258ef594481f94cff5fe7607e103f45d65b37e
kawgh1 <krseven@gmail.com>
2022-03-05 16:21:41 -0600

    04-accordion

--- 04-accordion/README.md
+++ 04-accordion/README.md
@@ -1,3 +1,8 @@
+-   This project uses Ai Icons which generates an error in the console
+-   "React does not recognize the `pId` prop on a DOM element."
+-   would use Font Awesome, otherwise it works fine
+-   might add a smooth animation for the dropdown / roll up
+
 #### Idea
 
 [uidesigndaily](https://uidesigndaily.com/posts/sketch-accordion-website-day-1175)
@@ -13,8 +18,8 @@ npm install react-icons --save
 ```
 
 ```javascript
-import { FaHome } from 'react-icons/fa';
+import { FaHome } from "react-icons/fa";
 const Component = () => {
-  return <FaHome className='icon'></FaHome>;
+    return <FaHome className="icon"></FaHome>;
 };
 ```
--- 04-accordion/src/App.js
+++ 04-accordion/src/App.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useEffect, useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+    const [questions, setQuestions] = useState(data || []);
+
+    useEffect(() => {
+        setQuestions(data);
+    }, []);
+
+    console.log(questions);
+    return (
+        <main>
+            <div className="container">
+                <h3>Questions & Answers</h3>
+                <section className="info">
+                    {questions.map((question) => (
+                        <SingleQuestion
+                            key={question.id}
+                            title={question.title}
+                            info={question.info}
+                        />
+                    ))}
+                </section>
+            </div>
+        </main>
+    );
 }
 
 export default App;
