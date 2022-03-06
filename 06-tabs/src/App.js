f31849fc40551f56f2c253e976ae02b50c7e3530
kawgh1 <krseven@gmail.com>
2022-03-05 18:29:36 -0600

    update

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -1,10 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  return <h2>tabs project setup</h2>
+    const [jobs, setJobs] = useState([]);
+
+    async function getJobs() {
+        try {
+            const response = await fetch(
+                "https://course-api.com/react-tabs-project"
+            );
+            const data = await response.json();
+            setJobs(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getJobs();
+    }, []);
+
+    console.log(jobs);
+
+    return (
+        <main>
+            <div>
+                {jobs.map((job) => (
+                    <div key={job.id}>
+                        <h1>{job.title}</h1>
+                    </div>
+                ))}
+            </div>
+        </main>
+    );
 }
 
-export default App
+export default App;
